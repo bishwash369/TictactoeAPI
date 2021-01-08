9fcb8f2d4b10a536139a94991a9efec2d4b88587
bishwash369 <bishwashparajuli10@gmail.com>
2021-01-09 01:30:34 +0545

    app complete

--- TictactoeApi/Controllers/MovesController.cs
+++ TictactoeApi/Controllers/MovesController.cs
@@ -15,8 +15,8 @@ namespace TictactoeApi.Controllers
     [ApiController]
     public class MovesController : ControllerBase
     {
-         public readonly ApplicationDbContext db;
-         //public int count;
+        public readonly ApplicationDbContext db;
+        //public int count;
 
         public MovesController(ApplicationDbContext DbContext)
         {
@@ -39,73 +39,71 @@ namespace TictactoeApi.Controllers
 
         [HttpPost]
         [Route("Move")]
-        public async Task<bool> Move([FromBody] MoveDto move)
+        public async Task<IActionResult> Move([FromBody] MoveDto move)
         {
-           
+            var count = db.Moves.Where(x => x.GameId == move.GameId).Count();
+            if (count >= 5)
+            {
+                var data = db.Moves.Where(x => x.GameId == move.GameId).Select(x => x.Position).ToList();
+                var value = db.Moves.Where(x => x.GameId == move.GameId && x.MoveType == move.MoveType).Select(x => x.Position).ToList();
+                var arr = db.Moves.Where(x => x.MoveType == move.MoveType).OrderBy(x => x.Position).ToList();
+
+                List<List<int>> test = new List<List<int>>
+                    {
+                       new List<int>() { 1, 2, 3 },
+                       new List<int>() { 4, 5, 6 },
+                       new List<int>() { 7, 8, 9 },
+                       new List<int>() { 1, 4, 7 },
+                       new List<int>() { 2, 5, 8 },
+                       new List<int>() { 3, 6, 9 },
+                       new List<int>() { 1, 5, 9 },
+                       new List<int>() { 3, 5, 7 },
+                    };
+
+                var allNumbers = from l in test
+                                 let numbers = l
+                                 from n in numbers
+                                 select n;
+                foreach (var n in allNumbers)
+                {
+                    foreach (var a in arr)
+                    {
+                        var v = n.Equals(a);
+                        if (move.MoveType == "x")
+                        {
+                            if (v)
+                            {
+                                return Ok("Winner is Player" + move.PlayerId);
+                            }
+                        }
+                        else if(move.MoveType == "x")
+                        {
+                            if (v)
+                            {
+                                return Ok("Winner is Player" + move.PlayerId);
+                            }
+                        }
+                        else
+                        {
+                            return Ok("Game ends in a Tie");
+                        }
+                    }
+                }
+            
+            }
             var myMove = new Move()
             {
                 MoveType = move.MoveType,
                 Position = move.Position,
                 PlayerId = move.PlayerId,
                 GameId = move.GameId,
-                //count++;
-                //return count;
             };
-
             await db.Moves.AddAsync(myMove);
             await db.SaveChangesAsync();
-            return true;
-
-
-            var count = db.Moves.Where(x => x.GameId == move.GameId).Count();
-            if (count >= 5)
-            {
-                var data = db.Moves.Where(x => x.GameId == move.GameId).Select(x => x.Position).ToArray();
-                var value = db.Moves.Where(x => x.GameId == move.GameId && x.MoveType == move.MoveType).Select(x => x.Position).ToArray();
-                var arr = db.Moves.Where(x => x.MoveType == move.MoveType).OrderBy(x => x.Position).ToList();
-                int[] arr1 =
-                    {
-                        { 1, 2, 3 },
-                        { 4, 5, 6 },
-                        { 7, 8, 9 },
-                        { 1, 4, 7 },
-                        { 2, 5, 8 },
-                        { 3, 6, 9 },
-                        { 1, 5, 9 },
-                        { 3, 5, 7 },
-                    };
-                
-                if (move.MoveType == "x")
-                {
-                    for(int i=0; i<arr1.Length;i++)
-                    { 
-                        if(arr == arr1[i])
-                        { 
-                            throw new ArgumentException("Winner is Player" + move.PlayerId);
-                        }
-                    }
-                } 
-                else if (move.MoveType == "o")
-                {
-                    for (int i = 0; i < arr1.Length; i++)
-                    {
-                        if (arr == arr1[i])
-                        {
-                            throw new ArgumentException("Winner is Player" + move.PlayerId);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new ArgumentException("Game ends in a Tie");
-                }
-                
-            }
-            else
-            {
-                throw new ArgumentException("Invalid");
-            }
+            return Ok(true);
 
         }
     }
+      
+
 }
